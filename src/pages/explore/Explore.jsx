ba885115f222ec9c3e800af9c27b7cb0ec2babbf
hebaosman443 <hebaosman443@gmail.com>
2023-07-24 03:15:35 +0300

    fixed error

--- src/pages/explore/Explore.jsx
+++ src/pages/explore/Explore.jsx
@@ -6,7 +6,7 @@ import Select from 'react-select'
 import './style.scss'
 
 import useFetch from '../../hooks/useFetch'
-import { FetchDataFromApi } from '../../utils/api'
+import { FetchDataFromApi } from '../../utils/Api'
 import ContentWrapper from '../../components/contentWrapper/ContentWrapper'
 import MovieCard from '../../components/movieCard/MovieCard'
 import Spinner from '../../components/spinner/Spinner'
